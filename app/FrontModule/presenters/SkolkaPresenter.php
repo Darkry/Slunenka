fe3912bc5b83738a666e6562cf8a1e9a7d97bfff
Darkry <darkry@dark-project.cz>
2015-01-26 18:48:35 +0100

    Adds Skolka pages

--- app/FrontModule/presenters/SkolkaPresenter.php
+++ app/FrontModule/presenters/SkolkaPresenter.php
@@ -13,4 +13,23 @@ class SkolkaPresenter extends BasePresenter
 		$this->template->text = $facade->getTextById(1);
 	}
 
+	public function renderProstredi() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(2);
+	}
+
+	public function renderDokumenty() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(3);
+	}
+
+	public function renderNovinky() {
+		
+	}
+
+	public function renderProbehlo() {
+		
+	}
+
+
 }
