75eeac764d8f27aeb9dec39059325dc367fb5dfd
jezzajeremy <jeremy.stepanek@icloud.com>
2015-01-28 16:52:00 +0100

    Adds Aktivity pages

---
+++ app/FrontModule/presenters/AktivityPresenter.php
@@ -0,0 +1,43 @@
+<?php
+namespace FrontModule;
+
+use Nette\Application\UI\Form;
+
+class AktivityPresenter extends BasePresenter
+{
+
+	public function renderNejmladsi() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(4);
+	}
+
+	public function renderPredskolni() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(5);
+	}
+
+	public function renderSkolaci() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(6);
+	}
+
+	public function renderStarsi() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(7);
+	}
+
+	public function renderTerapie() {
+		$facade = $this->getContext()->textFacade;
+		$this->template->text = $facade->getTextById(8);
+	}
+
+	public function renderNovinky() {
+
+	}
+
+	public function renderProbehlo() {
+
+	}
+
+
+}
