2ff9ed8d73907342f3e6edb79f4b4bd2e56b7bf1
Darkry <darkry@dark-project.cz>
2015-02-04 16:20:20 +0100

    Finally commit

--- app/models/facades/ListFacade.php
+++ app/models/facades/ListFacade.php
@@ -54,6 +54,10 @@ class ListFacade {
         return $this->itemRep->find($id);
     }
 
+    public function getAllItems() {
+        return $this->itemRep->findAll();
+    }
+
     public function deleteItem($id) {
         $item = $this->getItem($id);
 
